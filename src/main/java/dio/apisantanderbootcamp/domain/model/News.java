9127e92ce29802175fa6a090249b16938b764e46
willianctti <willianctti@gmail.com>
2023-10-16 11:45:53 -0300

    implementação da camada de domínio
    Signed-off-by: willianctti <willianctti@gmail.com>

---
+++ src/main/java/dio/apisantanderbootcamp/domain/model/News.java
@@ -0,0 +1,11 @@
+package dio.apisantanderbootcamp.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
---
+++ src/main/java/dio/apisantanderbootcamp/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dio.apisantanderbootcamp.domain.repository;
+
+import dio.apisantanderbootcamp.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
