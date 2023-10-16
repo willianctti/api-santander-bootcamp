4a9a5565a4c0d66c6a1164826718404cbe8b6b19
willianctti <willianctti@gmail.com>
2023-10-16 10:02:36 -0300

    Initial commit

---
+++ src/main/java/dio/apisantanderbootcamp/ApiSantanderBootcampApplication.java
@@ -0,0 +1,13 @@
+package dio.apisantanderbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSantanderBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSantanderBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/apisantanderbootcamp/ApiSantanderBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.apisantanderbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSantanderBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
